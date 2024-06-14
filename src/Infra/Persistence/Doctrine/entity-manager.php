71f60df6ff4c17429193801b3b2b07cda812a166
Luiz Moura <moura.oliveira.luiz@gmail.com>
2024-06-14 16:12:32 -0400

    feat: creates connection to the database

---
+++ src/Infra/Persistence/Doctrine/entity-manager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Infra\Persistence\Doctrine;
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+$config = ORMSetup::createAttributeMetadataConfiguration(
+    paths: [__DIR__.'/Entities'],
+    isDevMode: $_ENV['APP_ENV'] === 'dev',
+);
+
+$connection = DriverManager::getConnection(
+    require_once __DIR__ . '/../../../../config/doctrine.php',
+    $config
+);
+
+return  new EntityManager($connection, $config);
